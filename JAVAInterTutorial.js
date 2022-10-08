4888ca72e531c7ba3682afbf41975b07f5810493
oneslaught <iceminecraft021@gmail.com>
2022-10-08 22:59:45 +0300

    JAVAInterTutorial.js

--- JAVAInterTutorial.js
+++ JAVAInterTutorial.js
@@ -1,5 +1,22 @@
 var handleClick = function (event) {
-    // do something!
+    // Что-то
 };
 var button = document.querySelector('#big-button');
 button.addEventListener('click', handleClick);
+
+
+var req = new XMLHttpRequest();
+req.onload = function (event) { /* Что-то */ };
+req.open('get', 'some-file.txt', true);
+req.send();
+
+var jsonString = JSON.stringify({
+    make: "Пицца",
+    model: "Муха",
+    miles: 8800
+});
+
+var car = JSON.parse(jsonString);
+car.model = "W.W";
+
+console.log(window.innerHeight);
