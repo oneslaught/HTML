9af935f5e76ac284465e1a20fec1e66c37f44a33
oneslaught <iceminecraft021@gmail.com>
2022-10-09 20:31:26 +0300

    JAVAInterTutorial.js

--- JAVAInterTutorial.js
+++ JAVAInterTutorial.js
@@ -20,3 +20,25 @@ var car = JSON.parse(jsonString);
 car.model = "W.W";
 
 console.log(window.innerHeight);
+
+var a = 10;
+
+if (a > 5) {
+    var b = 5;
+}
+
+var c = a + b;
+
+var doSomething = function () {
+    var a = 10;
+var doSomethingElse = function () {
+    console.log(a);
+};
+doSomethingElse();
+};
+
+doSomething();
+
+$('#btn').click(function () {
+    // Что-то
+});
