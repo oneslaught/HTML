67236ce7399ccc10cf4998fd19ef6bfcf915728b
oneslaught <iceminecraft021@gmail.com>
2022-11-09 23:08:03 +0200

    jjj

--- jjj.js
+++ jjj.js
@@ -1,17 +1,30 @@
 function addPost(post) {
-    const table = $("#aaa");
-    const tr = document.createElement("tr");
-    console.log(post);
-    const idCell = document.createElement("td");
-    idCell.innerText = post.id;
-    tr.appendChild(idCell);
-    const titleCell = document.createElement("td");
-    titleCell.innerText = post.title || "";
-    tr.appendChild(titleCell);
-    const authorCell = document.createElement("td");
-    authorCell.innerText = post.author || "";
-    tr.appendChild(authorCell);
-    table.append(tr);
+  const table = $("#aaa");
+  const tr = document.createElement("tr");
+  console.log(post);
+  const idCell = document.createElement("td");
+  idCell.innerText = post.id;
+  tr.appendChild(idCell);
+  const titleCell = document.createElement("td");
+  titleCell.innerText = post.title || "";
+  tr.appendChild(titleCell);
+  const authorCell = document.createElement("td");
+  authorCell.innerText = post.author || "";
+  tr.appendChild(authorCell);
+  table.append(tr);
+  const btn = document.createElement("button");
+  btn.id = "btn";
+  tr.appendChild(btn);
+  btn.onclick = function () {
+    tr.removeChild(idCell);
+    tr.removeChild(titleCell);
+    tr.removeChild(authorCell);
+    tr.removeChild(btn);
+    $.ajax({
+      url: 'http://localhost:3000/posts/',
+      type: 'DELETE',
+    });
+  };
 }
 function custom(e) {
   e.preventDefault();
@@ -19,13 +32,13 @@ function custom(e) {
   const post = Object.fromEntries(form.entries());
   console.log(post);
   $.post("http://localhost:3000/posts", post, function (response) {
-    addPost(post)
+    addPost(post);
   });
 }
 window.onload = function () {
   $.get("http://localhost:3000/posts", function (data) {
     data.forEach((post) => {
-    addPost(post)
+      addPost(post);
     });
   });
 };
