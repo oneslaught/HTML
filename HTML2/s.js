d807f84fd97a571453b2e079ac3e1dfe3f640bf1
oneslaught <iceminecraft021@gmail.com>
2023-01-03 22:42:47 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -9,14 +9,51 @@
 // const OurPlanetName = "Earth"
 // let currentUserName = "Babijon"
 
-let userName = prompt("Как вас зовут?")
-let noMama = "У вас нет имени? а как насчёт мамы?"
-if (userName === null || userName === "") {
-    alert(noMama)
-} 
-else {
-    alert(`Вас зовут ${userName}!`)
-}
+// let userName = prompt("Как вас зовут?")
+// if (userName === null || userName === "") {
+//     alert("У вас нет имени? а как насчёт мамы?")
+// } 
+// else {
+//     alert(`Вас зовут ${userName}!`)
+// }
+
+// let jsName = prompt("Какое «официальное» название JavaScript?", "")
+// if (jsName == "ECMAScript") {
+//     alert("Верно!")
+// }
+// else if (jsName.toLowerCase() === "ecmascript") {
+//     alert("Верно!")
+// }
+// else {
+//     alert("Не знаете? ECMAScript!")
+// }
+
+// let number = (prompt("Введите число", ""))
+// if (number > 0) {
+//     alert(1)
+// }
+// else if (number < 0) {
+//     alert(-1)
+// }
+// else if (number == 0) {
+//     alert(0)
+// }
+// else {
+//     alert("Циферки это 1,2,3...")
+// }
+
+// let a = 2
+// let b = 12
+// let result = (a + b < 4) ? 'Мало': 'Много';
+// alert(result)
+
+// let message = (login == 'Сотрудник') ? 'Привет' :
+//   (login == 'Директор') ? 'Здравствуйте' :
+//   (login == '') ? 'Нет логина' :
+//   '';
+
+
+
 
 
 
