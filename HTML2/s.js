c14760cea493a9dfd5489e78007163bf0b1bd647
oneslaught <iceminecraft021@gmail.com>
2023-01-06 23:48:53 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -115,5 +115,35 @@
 //   alert( i ); 
 // }
 
+// let browserName = prompt("Enter browser name")
+// if (browserName == "Edge") {
+//     alert( "You've got the Edge!" )
+// }
+// else if (browserName == "Chrome" || browserName == "Firefox" || browserName == "Safari" || browserName == "Opera") {
+//     alert( 'Okay we support these browsers too' )
+// }
+// else if (!browserName) {
+//     alert("Why are you gay?")
+// }
+// else {
+//     alert( 'We hope that this page looks ok!' )
+// }
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// switch(number) {
+//     case 0:
+//         alert('Вы ввели число 0');
+//         break;
+//     case 1:
+//         alert('Вы ввели число 1');
+//         break
+//     case 2:
+//     case 3:
+//         alert('Вы ввели число 2, а может и 3');
+//         break;
+//     default:
+//         alert('Вы делаете что-то не то в своей жизни')
+// }
+
 
 
