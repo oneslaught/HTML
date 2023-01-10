a4828bfffe3d5b21655779926f3c294e793d5eaf
oneslaught <iceminecraft021@gmail.com>
2023-01-10 23:07:14 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -214,4 +214,36 @@
 //     } else {
 //         alert( pow(x,n) );
 //     }
+
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name;
+
+// function isEmpty(obj) {
+//     obj = schedule
+//     if (Object.keys(obj).length) {
+//         alert(false)
+//     } else {
+//         alert(true)
+//     }
+// }
+
+// let schedule = {};
+// alert(isEmpty(schedule))
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// let sum = 0;
+// for (let prop in salaries) {
+//   sum += salaries[prop]
+// }
+
+// alert(`Общая сума команды ${sum}!`)
+
     
