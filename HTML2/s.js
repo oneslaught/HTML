77ba451797f5aacd902189982d9858592d45867d
oneslaught <iceminecraft021@gmail.com>
2023-01-11 17:14:49 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -246,4 +246,21 @@
 
 // alert(`Общая сума команды ${sum}!`)
 
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: "My menu"
+// };
+
+// function multiplyNumeric(obj) {
+//     for (let key in obj) {
+//       if (typeof obj[key] == 'number') {
+//       obj[key] *= 2;
+//     }
+//     alert(obj[key])
+//     }
+// }
+
+// multiplyNumeric(menu);
+
     
