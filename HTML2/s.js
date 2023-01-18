ab73f934bf76c06800c36797e3e99fc6cd48f761
oneslaught <iceminecraft021@gmail.com>
2023-01-18 21:41:11 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -407,3 +407,10 @@
 // }
 
 // alert( extractCurrencyValue("$120") )
+
+// let styles = ["Джаз", "Блюз"]
+// styles.push("Рок-н-ролл")
+// styles[Math.floor((styles.length - 1) / 2)] = "Классика"
+// alert(styles.shift())
+// styles.unshift("Рэп", "Регги")
+
