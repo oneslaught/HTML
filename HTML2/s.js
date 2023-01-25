4e8c4d50fff3e36670d4f0b676b31a9ef347b587
oneslaught <iceminecraft021@gmail.com>
2023-01-25 19:58:23 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -635,3 +635,29 @@ alert(names.slice()) на 3 вызова*/
   
 // let usersById = groupById(users);
 // console.log(usersById)
+
+// function unique(arr) {
+//     let res = Array.from(new Set(arr))
+//     return res
+// }
+
+// let values = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+
+// alert( unique(values) );
+
+// function aclean(arr) {
+//     let map = new Map()
+  
+//     for (let word of arr) {
+//       let sorted = word.toLowerCase().split("").sort().join("")
+//       map.set(sorted, word)
+//     }
+//     return Array.from(map.values())
+//   }
+  
+//   let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  
+//   alert( aclean(arr) );
+
