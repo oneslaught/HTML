87e46b4537489dd4e93fa9ae1fc5e76c7785a2a8
oneslaught <iceminecraft021@gmail.com>
2023-01-27 19:39:01 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -682,4 +682,30 @@ alert(names.slice()) на 3 вызова*/
 //     { text: "See you soon", from: "Alice" }
 // ];
 
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250
+// };
+  
+// function sumSalaries(salaries) {
+//     let sum = 0
+//     for (let salary of Object.values(salaries)) {
+//         sum += salary
+//     }
+//     return sum
+// }
+
+// console.log( `Amount of salaries: ${sumSalaries(salaries)}` );
+
+// let user = {
+//     name: 'John',
+//     age: 30
+// };
+
+// function count(obj) {
+//     return Object.keys(obj).length
+// }
+
+// alert( count(user))
 
