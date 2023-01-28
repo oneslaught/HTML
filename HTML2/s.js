cf2904e0f191d1dfe4df84d4959b243e1497b069
oneslaught <iceminecraft021@gmail.com>
2023-01-28 23:15:39 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -709,3 +709,33 @@ alert(names.slice()) на 3 вызова*/
 
 // alert( count(user))
 
+// let user = {
+//     name: "John",
+//     years: 30
+// };
+
+// let {name, years: age, isAdmin = false} = user
+
+// alert( name );
+// alert( age );
+// alert( isAdmin );
+
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250
+// };
+
+// function topSalary(salaries) {
+//     let max = 0
+//     let maxName = null
+  
+//     for (let [name, salary] of Object.entries(salaries)) {
+//       if (max < salary) {
+//         max = salary
+//         maxName = name
+//       }
+//     }
+//     return maxName
+// }
+
