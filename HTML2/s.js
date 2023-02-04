81152e9bb5f013e46dd53d8b6eaea748752c805c
oneslaught <iceminecraft021@gmail.com>
2023-02-04 23:57:19 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -873,3 +873,10 @@ alert(names.slice()) на 3 вызова*/
 // }
 
 // console.log(sumTo(6))
+
+// function factorial(n) {
+//     if (n == 1) return 1
+//     return n * factorial(n - 1)
+// }
+
+// console.log(factorial(5))
