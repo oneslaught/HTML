090b77a73ca749475c8999ce3a3d2ffc73a428c2
oneslaught <iceminecraft021@gmail.com>
2023-02-13 23:03:49 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -1180,3 +1180,40 @@ console.log(fib(89)) */
 
 //   return wrapper;
 // }
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", '');
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: 'Вася',
+
+//   loginOk() {
+//     alert(`${this.name} logged in`);
+//   },
+
+//   loginFail() {
+//     alert(`${this.name} failed to log in`);
+//   },
+
+// };
+
+// askPassword.bind(user.loginOk.bind(user), user.loginFail.bind(user));
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", '');
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: 'John',
+
+//   login(result) {
+//     alert( this.name + (result ? ' logged in' : ' failed to log in') );
+//   }
+// };
+
+// askPassword(user.login.bind(user, true), user.login.bind(user, false));
