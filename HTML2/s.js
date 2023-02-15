680d3b788efc20e3345f3e6d8b48a5d5fbbd6b06
oneslaught <iceminecraft021@gmail.com>
2023-02-15 20:17:49 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -1217,3 +1217,49 @@ console.log(fib(89)) */
 // };
 
 // askPassword(user.login.bind(user, true), user.login.bind(user, false));
+
+// let head = {
+//   glasses: 1,
+// };
+
+// let table = {
+//   pen: 3,
+//   __proto__: head
+// };
+
+// let bed = {
+//   sheet: 1,
+//   pillow: 2,
+//   __proto__: table
+// };
+
+// let pockets = {
+//   money: 2000,
+//   __proto__: bed
+// };
+
+// alert(pockets.pen)
+// alert(bed.glasses)
+
+// let hamster = {
+//   stomach: [],
+
+//   eat(food) {
+//     this.stomach.push(food);
+//   }
+// };
+
+// let speedy = {
+//   __proto__: hamster,
+//   stomach: [],
+// };
+
+// let lazy = {
+//   __proto__: hamster,
+//   stomach: [],
+// };
+
+// speedy.eat("apple");
+
+// alert( speedy.stomach );
+// alert( lazy.stomach );
