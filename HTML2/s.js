1073629790b4c858fc52d7240ce07c785c7d8530
oneslaught <iceminecraft021@gmail.com>
2023-02-20 23:56:11 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -1341,3 +1341,76 @@ console.log(fib(89)) */
 
 // let clock = new Clock({template: 'h:m:s'});
 // clock.start();
+
+// class Animal {
+
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+// }
+
+// class Rabbit extends Animal {
+//   constructor(name) {
+//     super(name)
+//     this.created = Date.now();
+//   }
+// }
+
+// let rabbit = new Rabbit("Белый кролик");
+// alert(rabbit.name);
+
+// class Clock {
+//   constructor({ template }) {
+//     this.template = template;
+//   }
+
+//   render() {
+//     let date = new Date();
+
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = '0' + mins;
+
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = '0' + secs;
+
+//     let output = this.template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+
+//     console.log(output);
+//   }
+
+//   stop() {
+//     clearInterval(this.timer);
+//   }
+
+//   start() {
+//     this.render();
+//     this.timer = setInterval(() => this.render(), 1000);
+//   }
+// }
+
+// class ExtendedClock extends Clock {
+//   constructor(options) {
+//     super(options)
+//     let { precision = 1000 } = options
+//     this.precision = precision
+//   }
+  
+//   start() {
+//     this.render()
+//     this.timer = setInterval(() => this.render(), this.precision)
+//   }
+// }
+
+// let clock = new ExtendedClock({
+//   template: "h:m:s",
+//   precision: 10000,
+// })
+
+// clock.start()
