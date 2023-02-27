0adb71817ca8f1283696924b8bfb1fe05dd0c120
oneslaught <iceminecraft021@gmail.com>
2023-02-27 20:16:38 +0200

    s.js

--- HTML2/s.js
+++ HTML2/s.js
@@ -1448,3 +1448,14 @@ console.log(fib(89)) */
 // }
 
 // delay(3000).then(() => alert('выполнилось через 3 секунды'));
+
+// document.body.firstElementChild
+// document.body.lastElementChild
+// document.body.lastElementChild.lastElementChild
+
+// table.rows[0].cells[0].style.background = 'red'
+// table.rows[1].cells[1].style.background = 'red'
+// table.rows[2].cells[2].style.background = 'red'
+// table.rows[3].cells[3].style.background = 'red'
+// table.rows[4].cells[4].style.background = 'red'
+
